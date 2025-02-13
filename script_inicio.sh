9eb5507e1eafdd2146cfe4fa15dce4988f8b60e5
DMarquesBR <dieguimb@gmail.com>
2025-02-13 13:47:41 -0300

    Update script_inicio.sh

--- script_inicio.sh
+++ script_inicio.sh
@@ -26,3 +26,14 @@ sudo usermod -aG docker $USER
 # Configure Docker to start on boot with systemd
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
+
+
+# Add Caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+
+sudo apt update
+sudo apt install caddy
+sudo apt remove iptables-persistent
+sudo apt autoremove
